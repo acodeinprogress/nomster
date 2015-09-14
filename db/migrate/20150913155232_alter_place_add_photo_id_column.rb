01c47c406e4de21df0d84dc663067953ef837320
Sharon Siegel <sjsieg830@gmail.com>
2015-09-14 00:48:01 +0000

    Uncomment adding photo id to place migration

--- db/migrate/20150913155232_alter_place_add_photo_id_column.rb
+++ db/migrate/20150913155232_alter_place_add_photo_id_column.rb
@@ -1,5 +1,5 @@
 class AlterPlacesAddPhotoIdColumn < ActiveRecord::Migration
-  #def change
-  #  add_column :places, :photo_id, :integer
-  #end
+  def change
+    add_column :places, :photo_id, :integer
+  end
 end
